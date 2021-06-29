f28c25713c23b983c0da276980623d64927a6ab9
jhonatan229 <jhonatan229@outlook.com>
2021-06-29 20:44:28 -0300

    fix return with null pointer

--- print_pointer.c
+++ print_pointer.c
@@ -6,7 +6,7 @@
 /*   By: jestevam < jestevam@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/29 14:45:05 by jestevam          #+#    #+#             */
-/*   Updated: 2021/06/29 20:01:30 by jestevam         ###   ########.fr       */
+/*   Updated: 2021/06/29 20:43:28 by jestevam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@ static int	count_print_places(unsigned long int num, char *base, int sinal)
 	int	len_base;
 
 	len_base = ft_strlen(base);
+	if (num == 0)
+		return (0);
 	count = 1;
 	if (num / len_base > 0)
 		count += count_print_places(num / len_base, base, sinal);
@@ -27,24 +29,32 @@ static int	count_print_places(unsigned long int num, char *base, int sinal)
 	return (count);
 }
 
-static void	print_pointer(unsigned long int numb)
+static void	print_pointer(unsigned long int numb, int s_dot)
 {
 	ft_putstr_fd("0x", 1);
-	count_print_places(numb, BASE_LOWER_HEXA, 1);
+	if (numb == 0)
+	{
+		if (s_dot == 0)
+			ft_putchar_fd('0', 1);
+	}
+	else
+		count_print_places(numb, BASE_LOWER_HEXA, 1);
 }
 
-static void print_right_left(int dif, unsigned long int numb, int sinal)
+static void print_right_left(int dif, unsigned long int numb, int sinal, int s_dot)
 {
 	if (sinal == 1)
 	{
-		print_pointer(numb);
+		print_pointer(numb, s_dot);
 		while (dif-- > 0)
 			ft_putchar_fd(' ', 1);
 	}
-	else 
+	else
+	{
 		while (dif-- > 0)
 			ft_putchar_fd(' ', 1);
-		print_pointer(numb);
+		print_pointer(numb, s_dot);
+	}
 }
 
 void	set_pointer(va_list list, t_flags *flag)
@@ -53,26 +63,18 @@ void	set_pointer(va_list list, t_flags *flag)
 	int	len_num;
 
 	numb = va_arg(list, unsigned long int);
-	if (numb == 0)
-	{
-		if (flag->dot)
-			ft_putstr_fd("0x", 1);
-		else
-			ft_putstr_fd("0x0", 1);
-		return ;
-	}
 	len_num = count_print_places(numb, BASE_LOWER_HEXA, 0) + 2;
 	if (flag->width > len_num)
 	{
 		flag->return_len += flag->width;
 		if (flag->sinal)
-			print_right_left(flag->width - len_num, numb, 1);
+			print_right_left(flag->width - len_num, numb, 1, flag->dot);
 		else
-			print_right_left(flag->width - len_num, numb, 0);
+			print_right_left(flag->width - len_num, numb, 0, flag->dot);
 	}
 	else
 	{
 		flag->return_len += len_num;
-		print_pointer(numb);
+		print_pointer(numb, flag->dot);
 	}
 }
