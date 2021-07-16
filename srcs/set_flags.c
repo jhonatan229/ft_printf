8be9f60bb4fb87d5d0148535f86aac9041b75923
jhonatan229 <jhonatan229@outlook.com>
2021-07-16 19:04:27 -0300

    commented files set_flags.c and utils.c

--- srcs/set_flags.c
+++ srcs/set_flags.c
@@ -6,12 +6,14 @@
 /*   By: jestevam < jestevam@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/22 17:33:45 by jestevam          #+#    #+#             */
-/*   Updated: 2021/07/16 13:56:31 by jestevam         ###   ########.fr       */
+/*   Updated: 2021/07/16 18:43:58 by jestevam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../ft_printf.h"
 
+//transform the number "str" in a integer
+//already populate flag width or precision depending if flag dot is on or not 
 static void	get_number(t_flags *flag)
 {
 	char	*str;
@@ -29,6 +31,7 @@ static void	get_number(t_flags *flag)
 	free(str);
 }
 
+//set the front flags
 static void	set_minus_zero(t_flags *flag)
 {
 	char	c;
@@ -49,21 +52,30 @@ static void	set_minus_zero(t_flags *flag)
 		flag->pos_str++;
 		c = flag->str[flag->pos_str];
 	}
+	//will put a char of the signal if flag plus or blank is on
+	//plus have priority
 	if (flag->plus)
 		flag->sinal_int = '+';
 	else if (flag->blank)
 		flag->sinal_int = ' ';
+	//between flag '-' or '0', the '-' is priority
 	if (flag->sinal && flag->zero)
 		flag->zero = 0;
 }
 
+//will transform the width in positive if needed,
+//and precision in 0 if needed
 static void	set_stars(t_flags *flag)
 {
+	//if width is smaller than 0, the flag sinal turn on
+	//width turn positive
 	if (flag->width < 0)
 	{
 		flag->sinal = 1;
 		flag->width *= -1;
 	}
+	//if precision is smaller than 0, the flag dot turn off
+	//precision turn 0;
 	if (flag->precision < 0)
 	{
 		flag->dot = 0;
@@ -71,13 +83,16 @@ static void	set_stars(t_flags *flag)
 	}
 }
 
+//will put in my struct "flag", the flags that appears
 void	pupulate_flags(va_list list, t_flags *flag)
 {
 	set_minus_zero(flag);
+	//percour the string "str" and going add on the struct the characters '.' and the numbers that appears
 	while (flag->str[flag->pos_str] != 0)
 	{
 		if (flag->str[flag->pos_str] == '.')
 			flag->dot = 1;
+		//this '*' will be convert by a number that coming in the parameter of the printf
 		else if (flag->str[flag->pos_str] == '*')
 		{
 			if (flag->dot)
--- srcs/utils.c
+++ srcs/utils.c
@@ -6,12 +6,15 @@
 /*   By: jestevam < jestevam@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/06 19:23:20 by jestevam          #+#    #+#             */
-/*   Updated: 2021/07/16 13:56:41 by jestevam         ###   ########.fr       */
+/*   Updated: 2021/07/16 19:03:40 by jestevam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../ft_printf.h"
 
+//serves to count or print(in hexadecimal base) depending of my sinal
+//if sinal = 1: print number and return the number of places
+//if sinal != 1: only return number of places
 int	count_print_pointer(unsigned long int num, char *base, int sinal)
 {
 	int	count;
@@ -26,6 +29,7 @@ int	count_print_pointer(unsigned long int num, char *base, int sinal)
 	return (count);
 }
 
+//serves to count(in hexadecimal base or decimal base).
 int	count_unsigned(unsigned int num, char *base, t_flags *flag)
 {
 	int	count;
@@ -40,6 +44,9 @@ int	count_unsigned(unsigned int num, char *base, t_flags *flag)
 	return (count);
 }
 
+//serves to count or print(in decimal base) depending of my sinal
+//if sinal = 1: print number and return the number of places
+//if sinal != 1: only return number of places
 int	count_print_int(int num, char *base, int sinal, int len_print)
 {
 	int	len_base;
@@ -47,7 +54,7 @@ int	count_print_int(int num, char *base, int sinal, int len_print)
 
 	count = 1;
 	len_base = ft_strlen(base);
-	if (num < 0 )
+	if (num < 0)
 	{
 		if (num == -2147483648)
 		{
@@ -64,6 +71,9 @@ int	count_print_int(int num, char *base, int sinal, int len_print)
 	return (count);
 }
 
+//will print the char c (diff) times,
+//if mark = 1: print char sinal before 'c'
+//if mark = 2: print char sinal after 'c'
 void	set_diff_int(int mark, int diff, char c, char sinal)
 {
 	if (mark == 1)
@@ -74,6 +84,7 @@ void	set_diff_int(int mark, int diff, char c, char sinal)
 		ft_putchar_fd(sinal, 1);
 }
 
+//print "0x" in the rigth place, is sinal is equal 2 or 3
 void	print_hash_hexa(int numb, int sinal, t_flags *flag, int len)
 {
 	int	wid;
