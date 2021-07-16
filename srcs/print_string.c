6c1fda8b5064f3d023480791e77141021a85adee
jhonatan229 <jhonatan229@outlook.com>
2021-07-16 18:23:52 -0300

    comment the files print_string.c and print_unsigned.c

--- srcs/print_string.c
+++ srcs/print_string.c
@@ -6,12 +6,13 @@
 /*   By: jestevam < jestevam@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/22 16:30:40 by jestevam          #+#    #+#             */
-/*   Updated: 2021/07/16 13:56:16 by jestevam         ###   ########.fr       */
+/*   Updated: 2021/07/16 17:41:29 by jestevam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../ft_printf.h"
 
+//align str depending of the flags that are ON
 static void	align_str(char *str, t_flags *flag)
 {
 	int	len;
@@ -35,13 +36,17 @@ static void	align_str(char *str, t_flags *flag)
 	return ;
 }
 
+//verify if will need print something more than "str"
 static void	print_str(char *str, t_flags *flag)
 {
+	//will align the str if the width is bigger than length of "str"
+	//if true, add width in the return length
 	if (flag->width > (int)ft_strlen(str))
 	{
 		flag->return_len += flag->width;
 		align_str(str, flag);
 	}
+	//otherwise, add the length of str in return_len and print str
 	else
 	{
 		flag->return_len += ft_strlen(str);
@@ -49,24 +54,29 @@ static void	print_str(char *str, t_flags *flag)
 	}
 }
 
+//will print a string, depending on my flags turned on.
 void	set_string(va_list list, t_flags *flag)
 {
 	char	*str;
 	char	*aux;
 
 	str = va_arg(list, char *);
+	//case the variable passed on printf is NULL, will be create a string with "(null)" writed
 	if (str == NULL)
 	{
 		str = ft_strdup("(null)");
+		//if there is precision, use substr for create another string with the length of precision (if is smaller than 6)
 		if (flag->dot)
 		{
 			aux = ft_substr(str, 0, flag->precision);
 			free(str);
 			str = aux;
 		}	
-	}	
+	}
+	//otherwise if there is precision, use substr for create another string with the length of precision (if is smaller than length of the string)
 	else if (flag->dot)
 		str = ft_substr(str, 0, flag->precision);
+	//otherwise put in "str" the value of variable received
 	else
 		str = ft_strdup(str);
 	print_str(str, flag);
--- srcs/print_unsigned.c
+++ srcs/print_unsigned.c
@@ -6,12 +6,14 @@
 /*   By: jestevam < jestevam@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/28 19:23:49 by jestevam          #+#    #+#             */
-/*   Updated: 2021/07/16 13:56:25 by jestevam         ###   ########.fr       */
+/*   Updated: 2021/07/16 18:20:39 by jestevam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../ft_printf.h"
 
+//print the number, with base on my length of char "base", that is a numeric base
+//print only the first (len) places
 static int	print_places(unsigned int num, char *base, int len, t_flags *flag)
 {
 	int	count;
@@ -26,6 +28,7 @@ static int	print_places(unsigned int num, char *base, int len, t_flags *flag)
 	return (count);
 }
 
+//will print the number, depending of the signal that is coming in the parameter
 static void	print_hashtag(int numb, int len_numb, t_flags *flag, int sinal)
 {
 	int	press;
@@ -53,19 +56,26 @@ static void	print_aling_rigth(char c, int len_numb, int numb, t_flags *flag)
 	int	len;
 	int	press;
 
+	//the press will receive precision if it is bigger than length numb
 	if (flag->precision > len_numb)
 		press = flag->precision;
 	else
 		press = len_numb;
+	//if the width is bigger than length of number plus flag hashtag(in case, if ON, will value 2(equivalent of "0x"))
+	//if true, the return_len receive this value
+	//the difference(len) receive width minus length of number plus flag hashtag
 	if (flag->width > press + flag->hashtag)
 	{
 		flag->return_len += flag->width;
 		len = flag->width - (press + flag->hashtag);
+		//this while will not work with c equals '0', because this will be treated in another function
 		while (len-- > 0 && c != '0')
 			ft_putchar_fd(c, 1);
 	}
+	//otherwise, the return_len receive length of number plus flag hashtag
 	else
 		flag->return_len += (press + flag->hashtag);
+	//will print the numb depending of type that is coming.
 	if (flag->str[flag->pos_str] == 'u')
 		print_hashtag(numb, len_numb, flag, 1);
 	else if (flag->str[flag->pos_str] == 'x')
@@ -74,18 +84,24 @@ static void	print_aling_rigth(char c, int len_numb, int numb, t_flags *flag)
 		print_hashtag(numb, len_numb, flag, 3);
 }
 
+//will align to left and populate the places with the flag that is ON
 static void	print_aling_left(char c, int len_numb, int numb, t_flags *flag)
 {
 	int	len;
 
+	//will print the numb depending of type that is coming.
 	if (flag->str[flag->pos_str] == 'u')
 		print_hashtag(numb, len_numb, flag, 1);
 	else if (flag->str[flag->pos_str] == 'x')
 		print_hashtag(numb, len_numb, flag, 2);
 	else if (flag->str[flag->pos_str] == 'X')
 		print_hashtag(numb, len_numb, flag, 3);
+	//the length numb will receive precision if it is bigger than it
 	if (flag->precision > len_numb)
 		len_numb = flag->precision;
+	//if the width is bigger than length of number plus flag hashtag(in case, if ON, will value 2(equivalent of "0x"))
+	//if true, the return_len receive this value
+	//the difference(len) receive width minus length of number plus flag hashtag
 	if (flag->width > len_numb + flag->hashtag)
 	{
 		flag->return_len += flag->width;
@@ -93,24 +109,29 @@ static void	print_aling_left(char c, int len_numb, int numb, t_flags *flag)
 		while (len-- > 0)
 			ft_putchar_fd(c, 1);
 	}
+	//otherwise, the return_len receive length of number plus flag hashtag
 	else
 		flag->return_len += (len_numb + flag->hashtag);
 }
 
+//will print a unsigned int, depending on my flags turned on.
 void	set_unsigned(va_list list, t_flags *flag)
 {
 	int				len;
 	unsigned int	numb;
 
 	numb = va_arg(list, unsigned int);
+	//will count the length of int depend of variable type
 	if (flag->str[flag->pos_str] == 'u')
 		len = count_unsigned(numb, BASE_DESC, flag);
 	else if (flag->str[flag->pos_str] == 'x')
 		len = count_unsigned(numb, BASE_LOWER_HEXA, flag);
 	else if (flag->str[flag->pos_str] == 'X')
 		len = count_unsigned(numb, BASE_UPPER_HEXA, flag);
+	//disable the flag hashtag if the numb is 0;
 	if (flag->hashtag && numb == 0)
 		flag->hashtag = 0;
+	//aligns right or left depending of wich flag is ON
 	if (flag->zero && !flag->dot)
 		print_aling_rigth('0', len, numb, flag);
 	else if (flag->sinal)
